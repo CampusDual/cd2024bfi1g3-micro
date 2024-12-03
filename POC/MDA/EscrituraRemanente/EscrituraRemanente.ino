25392e5bd53004e2373700b6cf6f33d0dd7c8101
Miguel Dagraza <migueldagraza@gmail.com>
2024-12-03 09:04:45 +0100

    escritura remanente probado

--- POC/MDA/EscrituraRemanente/EscrituraRemanente.ino
+++ POC/MDA/EscrituraRemanente/EscrituraRemanente.ino
@@ -14,26 +14,30 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Opciones [Escribir: E | Leer: L | Mostrar: M]");
   if (Serial.available() > 0) {
     String textoSerial = Serial.readStringUntil('\n');
 
     if (textoSerial.equalsIgnoreCase("E")) {
+      Serial.println("Seleccionado escribir.");
       escribir(wifi,0);
       escribir(passWifi,1);
     } else if (textoSerial.equalsIgnoreCase("L")) {
+      Serial.println("Seleccionado leer.");
       //leer();
     } else if (textoSerial.equalsIgnoreCase("M")) {
+      Serial.println("Seleccionado mostrar.");
       mostrar();
     }
   }
 
-  delay(2000);
+  delay(500);
 }
 
 void escribir(String frase, int posicion){
   byte num;
   for(int i=0;i<=posicion;i++){
-    if(i=0){
+    if(i==0){
       direccion=0;
     }else{
       EEPROM.get(direccion,num);
@@ -54,15 +58,23 @@ String leer(int posicion){
   byte num;
   String resultado="";
   for(int i=0;i<=posicion;i++){
-    if(i=0){
+    if(i==0){
       direccion=0;
     }else{
       EEPROM.get(direccion,num);
       direccion+=num+1;
     }
   }
+  //Obtener tamaño de dato a leer
+  EEPROM.get(direccion,num);
+  direccion++;
 
-
+  for (int i = 0; i < num; i++) {
+    char c;
+    EEPROM.get(direccion, c);
+    direccion++;
+    resultado += c;
+  }
 
  return resultado;
 }
