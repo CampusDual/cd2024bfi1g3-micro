d7302ed8dd1bf9792a360f3ce69e7ba3de5d8ff6
Gabriel <gabriel.lamela.ch@gmail.com>
2024-12-04 13:36:02 +0100

    Crear rutas en app-routing-module Corregir conflictos de Merge

--- AngularPruebas/test-app/src/app/app.module.ts
+++ AngularPruebas/test-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { TresRayaComponent } from './tres-raya/tres-raya.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    TresRayaComponent
   ],
   imports: [
     BrowserModule,
